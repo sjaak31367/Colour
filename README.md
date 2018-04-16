e66698cee4c42b70e64f47fd1ed108c4d0c1cf67
sjaak31367 <sjaak31367@gmail.com>
2018-04-16 14:10:48 +0200

    Added PerlinNoise
    Also: easier configuring to fit print to terminal

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ## Colour!
 This is just a small C++ thingy to try and print stuff in colour to a terminal.  
 
-Note that this does require a terminal (emulator) that supports 24-bits colour. (I'm using gnome-terminal)  
+Note that this does require a terminal (emulator) that supports 24-bits colour.
+(I'm using gnome-terminal, but there are tons of others out there)
+
+###### Credits:
+PerlinNoise.hpp: [github.com/Reputeless/PelinNoise](https://github.com/Reputeless/PerlinNoise)
 
 ###### Building:
-``g++ *.cpp -std=c++11 -o ./a.out``
+``g++ -std=c++11 -o ./a.out Colour.cpp main.cpp``
 
 ###### Running: 
 ``./a.out``
--- main.cpp
+++ main.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <vector>
+#include "PerlinNoise.hpp"
 #include "Colour.h"
 
 int main()
 {
+  #define TERM 1
+
+  #if TERM == 0
+  static const std::vector<int> terminalSize = {78, 62}; // School terminal size
+  #endif // TERM == 0
+
+  #if TERM == 1
+  static const std::vector<int> terminalSize = {105, 50}; // Personal Raspi terminal size (1920 x 1017)
+  #endif // TERM == 1
+
   std::vector<Colour> colours(255 * 255, Colour());
+  const siv::PerlinNoise perlin(3512);
+  int temp;
 
   // Generator
   for (int y = 0; y < 255; ++y) {
     for (int x = 0; x < 255; ++x) {
-      colours[y * 255 + x].set(x, y, ((x + y) / 2 - 128) * -1 + 127);
+      // PerlinNoise(locationX [, locationY [, locationZ]], octave): returns double 0 ~ 1 (-0.04 ~ 0.96)
+      temp = static_cast<int>( perlin.octaveNoise0_1(x / 32.f, y / 32.f, 8) * 256 );
+      colours[y * 255 + x].set(temp, temp, temp);
+      /* ColourMap / ColourWheel generator
+      colours[y * 255 + x].set(x, y, ((x + y) / 2 - 128) * -1 + 127); */
     }
   }
 
   // 0: print RGB seperate
   // 1: print RGB combined
-  #define MODE 1
+  #define PRINTMODE 1
 
-  #if MODE == 0
+  #if PRINTMODE == 0
   // RED
   for (int y = 0; y < 255; y += 10) {
     for (int x = 0; x < 255; x += 10) {
@@ -41,17 +58,17 @@ int main()
     }
     std::cout << std::endl;
   }
-  #endif // MODE == 0
+  #endif // PRINTMODE == 0
 
-  #if MODE == 1
+  #if PRINTMODE == 1
   // RGB
-  for (int y = 0; y < 255; y += 5) {
-    for (int x = 0; x < 255; x += 5) {
-      std::cout << colours[y * 255 + x] << "██";
+  for (float y = 0; y < 255; y += (255.f / terminalSize[1])) {
+    for (float x = 0; x < 255; x += (255.f / terminalSize[0])) {
+      std::cout << colours[static_cast<int>(y) * 255 + static_cast<int>(x)] << "██";
     }
     std::cout << std::endl;
   }
-  #endif // Mode == 1
+  #endif // PRINTMODE == 1
 
   return 0;
 }
